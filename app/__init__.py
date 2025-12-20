919a11b3fcab971007f6c8eaf36ae53e32ff179c
Sivad80 <158787478+Sivad80@users.noreply.github.com>
2025-12-20 14:03:25 +0000

    Add database setup with SQLAlchemy and Migrations

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,30 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 
+from .extensions import db, migrate
+
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
 
+    # Ensure Instance Folder Exists
+    os.makedirs(app.instance_path, exist_ok=True)
+
     # Basic Config
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-secret')
 
+    # SQLite DB Inside /instance/app.db
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(app.instance_path, "app.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     from .routes import main
     app.register_blueprint(main)
+
+    # Import Models So Flask-Migrate Can "See" Them
+    from . import models # noqa: F401
 
     return app
---
+++ app/models.py
@@ -0,0 +1,26 @@
+from datetime import date
+from .extensions import db
+
+class Bill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(120), nullable=False)
+    category = db.Column(db.String(50), nullable=False, default="Other")
+
+    amount = db.Column(db.Numeric(10, 2), nullable=False)
+    due_day = db.Column(db.Integer, nullable=False) # 1-31
+
+    is_active = db.Column(db.Boolean, nullable=False, default=True)
+    create_at = db.Column(db.Date, nullable=False, default=date.today)
+
+
+class Paycheck(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    source = db.Column(db.String(120), nullable=False) # e.g. Job, Side Gig
+    amount = db.Column(db.Numeric(10, 2), nullable=False)
+
+    pay_date = db.Column(db.Date, nullable=False)
+
+    created_at = db.Column(db.Date, nullable=False, default=date.today)
+    
